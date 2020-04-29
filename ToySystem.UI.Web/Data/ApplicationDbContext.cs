c8f3e39de05ccaff14ad03ec3060bf58843f2251
newtein <newtein80@gmail.com>
2020-04-29 15:56:11 +0900

    init

---
+++ ToySystem.UI.Web/Data/ApplicationDbContext.cs
@@ -0,0 +1,21 @@
+﻿using ToySystem.UI.Web.Models;
+using IdentityServer4.EntityFramework.Options;
+using Microsoft.AspNetCore.ApiAuthorization.IdentityServer;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ToySystem.UI.Web.Data
+{
+    public class ApplicationDbContext : ApiAuthorizationDbContext<ApplicationUser>
+    {
+        public ApplicationDbContext(
+            DbContextOptions options,
+            IOptions<OperationalStoreOptions> operationalStoreOptions) : base(options, operationalStoreOptions)
+        {
+        }
+    }
+}
