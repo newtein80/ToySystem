6fccb46eac25d60161be2aaeb91024db6a24295c
parkjongsoo <newtein80@gmail.com>
2020-05-06 09:04:41 +0900

    2020.05.06.Home.Commit

---
+++ ToySystem.Core/Models/Description_GenericMethod.md
@@ -0,0 +1,16 @@
+﻿# Generic
+
+- 클래스를 사용할 타입을 클래스를 디자인할 때 지정하는게 아니라 클래스를 사용할 때 지정한 후 사용하는 기술
+
+######
+[Reference Link 1](https://funfunhanblog.tistory.com/71)
+[Reference Link 2](https://bm4904.tistory.com/70)
+[Reference Link 3](https://morm.tistory.com/178?category=732836)
+[Reference Link 4](https://slaner.tistory.com/122)
+[Reference Link 5](https://slaner.tistory.com/126?category=546234)
+[Reference Link 6](https://adibong.tistory.com/entry/%ED%8E%8C-c-%EC%A0%9C%EB%84%A4%EB%A6%AD-%EC%99%84%EB%B2%BD%EC%A0%95%EB%A6%AC%ED%95%B4%EB%B3%B4%EC%9F%88)
+[Reference Link 7](https://centbin-dev.tistory.com/4)
+[Reference Link 7](http://blog.naver.com/PostView.nhn?blogId=curlicu&logNo=40139910788&parentCategoryNo=&categoryNo=&viewDate=&isShowPopularPosts=false&from=postView)
+[Reference Link 7](https://slaner.tistory.com/126)
+[Reference Link 7](https://qzqz.tistory.com/214)
+[Reference Link 7](https://qzqz.tistory.com/213?category=752329)
--- ToySystem.Core/Notifications/DomainNotificationHandler.cs
+++ ToySystem.Core/Notifications/DomainNotificationHandler.cs
@@ -8,6 +8,9 @@ using System.Threading.Tasks;
 
 namespace ToySystem.Core.Notifications
 {
+    /// <summary>
+    /// DomainNotification 클래스에 대한 handler
+    /// </summary>
     public class DomainNotificationHandler : INotificationHandler<DomainNotification>
     {
         private List<DomainNotification> _notifications;
@@ -17,6 +20,12 @@ namespace ToySystem.Core.Notifications
             _notifications = new List<DomainNotification>();
         }
 
+        /// <summary>
+        /// DomainNotification 리스트에 등록
+        /// </summary>
+        /// <param name="message"></param>
+        /// <param name="cancellationToken"></param>
+        /// <returns></returns>
         public Task Handle(DomainNotification message, CancellationToken cancellationToken)
         {
             _notifications.Add(message);
@@ -29,14 +38,92 @@ namespace ToySystem.Core.Notifications
             return _notifications;
         }
 
+        /// <summary>
+        /// DomainNotification 이 있을경우 true 반환
+        /// </summary>
+        /// <returns></returns>
         public virtual bool HasNotifications()
         {
             return GetNotifications().Any();
         }
 
+        /// <summary>
+        /// DomainNotification 초기화
+        /// </summary>
         public void Dispose()
         {
             _notifications = new List<DomainNotification>();
         }
     }
 }
+
+/*
+// 사용 방법
+public class NewUser : INotification  
+{  
+    public string Username { get; set; }  
+    public string Password { get; set; }  
+} 
+
+
+public class NewUserHandler : INotificationHandler<NewUser>  
+{  
+    public Task Handle(NewUser notification, CancellationToken cancellationToken)  
+    {  
+        //Save to log  
+        Debug.WriteLine(" ****  Save user in database  *****");  
+        return Task.FromResult(true);  
+    }  
+}
+
+
+public class EmailHandler : INotificationHandler<NewUser>  
+{  
+    public Task Handle(NewUser notification, CancellationToken cancellationToken)  
+    {  
+        //Send email  
+        Debug.WriteLine(" ****  Email sent to user  *****");  
+        return Task.FromResult(true);  
+    }  
+}  
+
+
+public class LogHandler : INotificationHandler<NewUser>  
+{  
+    public Task Handle(NewUser notification, CancellationToken cancellationToken)  
+    {  
+        //Save to log  
+        Debug.WriteLine(" ****  User save to log  *****");  
+        return Task.FromResult(true);  
+    }  
+}
+
+
+public class AccountsController : Controller  
+{  
+    private readonly IMediator _mediator;  
+    public AccountsController(IMediator mediator)  
+    {  
+        _mediator = mediator;  
+    }  
+
+    [HttpGet]  
+    public ActionResult Login()  
+    {  
+        return View();  
+    }  
+
+    [HttpGet]  
+    public ActionResult Register()  
+    {  
+        return View();  
+    }  
+  
+    [HttpPost]  
+    public ActionResult Register(NewUser user)  
+    {  
+        _mediator.Publish(user);  
+        return RedirectToAction("Login");  
+    }  
+}
+*/
